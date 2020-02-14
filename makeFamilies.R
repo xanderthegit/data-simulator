cd5e1db3f07f76aa498abdfa6939db2d7d441fb1
Xander Brehm <awesomexander@gmail.com>
2020-02-14 17:37:33 -0500

    makeFamilies.R
    Included more functions from the package Charlatan so that families have last names and individuals in those families have first names.

--- makeFamilies.R
+++ makeFamilies.R
@@ -1,10 +1,13 @@
 makeFamilies <- function(nFamilies, averageSize) {
   require(simstudy)
+  require(charlatan)
   gen.family <- defData(varname = "size", dist = "noZeroPoisson", 
                         formula = averageSize, id = "idFam")
   dtFam <- genData(nFamilies, gen.family)
+  dtFam$lastname <- replicate(nFamilies, persons$last_name())
   gen.Indiv <- defDataAdd(varname = "c0", dist = "normal", formula = 0, variance = 2)
   dtIndiv <- genCluster(dtFam, "idFam", numIndsVar = "size", level1ID = "idInd")
   dtIndiv <- addColumns(gen.Indiv, dtIndiv)
+  dtIndiv$firstname <- replicate(length(dtIndiv$idInd), persons$first_name())
   return(dtIndiv)
 }
